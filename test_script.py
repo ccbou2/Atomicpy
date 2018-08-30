d2b57d3e66707dbcfa56c26b8cbfaea586f658c2
ccbou2 <42015359+ccbou2@users.noreply.github.com>
2018-08-30 15:07:48 +1000

    Minor tweaks
    Have added low pass frequency to parameter files and simplified output param writing process by simply copying the input file and naming it with the timestamp accordingly. This also allows for a variety of parameter input files with some only including certain params and not needing entries for all.

--- test_script.py
+++ test_script.py
@@ -3,12 +3,13 @@
 from operators import *
 from qChain import *
 from utility import *
+from demodulating_faraday_file import *
+from shutil import copyfile
 import numpy as np 
 import time
 import matplotlib.pyplot as plt
 import git
 import yaml
-from demodulating_faraday_file import *
 
 # use .yaml filetype for parameters, python package needed.
 
@@ -18,9 +19,9 @@ dirtyRepo = repo.is_dirty()
 
 # If repository is dirty i.e. out of date, ask for input to continue and exit if desired
 if dirtyRepo is True:
-  status = input("Script has changed since last commit, continue? (y/n): ")
-  if status is 'n':
-    raise SystemExit
+	status = input("Script has changed since last commit, continue? (y/n): ")
+	if status is 'n':
+		raise SystemExit
 
 # Take current commit ID and print
 sha = repo.head.object.hexsha
@@ -30,140 +31,129 @@ print('Current commit version is ' + str(commitID))
 # Run main script
 if __name__ == "__main__":
 
-    # Open desired .yaml parameter file, set by nameParams, and read data to yamlParams
-    nameParams = 'test'
-    fNameParams = 'C:/Users/Boundsy/Documents/GitHub/Atomicpy/ParameterFiles/' \
-       + nameParams + '_params.yaml'
-    paramStream = open(fNameParams, 'r')
-    yamlParams = yaml.load(paramStream)
-    paramStream.close()
+		# Open desired .yaml parameter file, set by nameParams, and read data to yamlParams
+		nameParams = 'test_v2'
+		fNameParams = 'C:/Users/Boundsy/Documents/GitHub/Atomicpy/ParameterFiles/' \
+			 + nameParams + '_params.yaml'
+		paramStream = open(fNameParams, 'r')
+		yamlParams = yaml.load(paramStream)
+		paramStream.close()
 
-    # Set frequencies from yamlParams
-    # sampling frequncy of unitary (Hz)
-    fs = yamlParams['stepFreq']
-    # time to evolve state for (seconds) 
-    eperiod = yamlParams['evolutionTime']
-    # bias frequency
-    f0 = yamlParams['biasFreq']
-    # detuning frequency
-    det = yamlParams['detuningFreq']
-    # rabi frequency
-    rabi = yamlParams['rabiFreq']
-    # phase offset
-    phi = yamlParams['phaseOffset']
-    # total frequency
-    f = f0 + det
+		# Set frequencies from yamlParams
+		# sampling frequncy of unitary (Hz)
+		fs = yamlParams['stepFreq']
+		# time to evolve state for (seconds) 
+		eperiod = yamlParams['evolutionTime']
+		# bias frequency
+		f0 = yamlParams['biasFreq']
+		# detuning frequency
+		det = yamlParams['detuningFreq']
+		# rabi frequency
+		rabi = yamlParams['rabiFreq']
+		# phase offset
+		phi = yamlParams['phaseOffset']
+		# total frequency
+		f = f0 + det
+		# low pass filter frequency
+		if 'lowPassFreq' in yamlParams:
+			fPass = yamlParams['lowPassFreq']
+			runDemod = True
+		else:
+			runDemod = False
 
-    # Legacy frequency parameter definitions, left for reference/use if yaml imports break
+		# Legacy frequency parameter definitions, left for reference/use if yaml imports break
 
-    # # sampling frequncy of unitary (Hz)
-    # fs = 1e8
-    # # time to evolve state for (seconds) 
-    # eperiod = 1e-4
+		# # sampling frequncy of unitary (Hz)
+		# fs = 1e8
+		# # time to evolve state for (seconds) 
+		# eperiod = 1e-4
 
-    # # Define frequencies for the interacting EM field, detuning, Rabi frequency & phase, all in Hz
-    # f0 = gyro;   # lab value for bias field splitting
-    # det = 0;    # lab value for detuning
-    # rabi = 1e4;    # lab value for Rabi frequency
-    # phi = 0;
-    # f = f0 + det;
+		# # Define frequencies for the interacting EM field, detuning, Rabi frequency & phase, all in Hz
+		# f0 = gyro;   # lab value for bias field splitting
+		# det = 0;    # lab value for detuning
+		# rabi = 1e4;    # lab value for Rabi frequency
+		# phi = 0;
+		# f = f0 + det;
 
-    # define B fields for lab frame
-    def Bx(t): return 2 * rabi * np.cos(2 * np.pi * f * t - phi)
-    def By(t): return 0 * (t/t)
-    def Bz(t): return (det - f) * (t/t)
+		# define B fields for lab frame
+		def Bx(t): return 2 * rabi * np.cos(2 * np.pi * f * t - phi)
+		def By(t): return 0 * (t/t)
+		def Bz(t): return (det - f) * (t/t)
 
-    # define B fields in first rotating frame
-    # def Bx(t): return rabi * np.cos(phi * (t/t))
-    # def By(t): return rabi * np.sin(phi * (t/t))
-    # def Bz(t): return det * (t/t)
+		# define B fields in first rotating frame
+		# def Bx(t): return rabi * np.cos(phi * (t/t))
+		# def By(t): return rabi * np.sin(phi * (t/t))
+		# def Bz(t): return det * (t/t)
 
-    # define generic Hamiltonian parameters with Zeeman splitting and rf dressing
-    params = {"struct": ["custom",           # Fx is a sinusoidal dressing field field
-                         "custom",           # Fy is a constant field
-                         "custom"],          # Fz is a fade field 
-              "freqb":  [0, 0, 0],           # frequency in Hz of each field vector
-              "tau":    [None, None, None],  # time event of pulse along each axis
-              "amp":    [0, 0, 0],           # amplitude in Gauss -> 1 Gauss ~= 700000 Hz precession
-              "misc":   [Bx, By, Bz]}        # misc parameters
+		# define generic Hamiltonian parameters with Zeeman splitting and rf dressing
+		params = {"struct": ["custom",           # Fx is a sinusoidal dressing field field
+												 "custom",           # Fy is a constant field
+												 "custom"],          # Fz is a fade field 
+							"freqb":  [0, 0, 0],           # frequency in Hz of each field vector
+							"tau":    [None, None, None],  # time event of pulse along each axis
+							"amp":    [0, 0, 0],           # amplitude in Gauss -> 1 Gauss ~= 700000 Hz precession
+							"misc":   [Bx, By, Bz]}        # misc parameters
 
-    # create specified magnetic fields and resulting Hamiltonian
-    fields = field_gen(field_params=params)
-    ham = Hamiltonian() 
-    ham.generate_field_hamiltonian(fields)
+		# create specified magnetic fields and resulting Hamiltonian
+		fields = field_gen(field_params=params)
+		ham = Hamiltonian() 
+		ham.generate_field_hamiltonian(fields)
 
-    # initialise spin 1/2 system in zero (down) state
-    atom = SpinSystem(init="zero")
+		# initialise spin 1/2 system in zero (down) state
+		atom = SpinSystem(init="zero")
 
-    # evolve state under system Hamiltonian
-    start = time.time()
-    tdomain, probs, pnts = atom.state_evolve(t=[1e-44, eperiod, 1/fs],          # time range and step size to evolve for
-                                          hamiltonian=ham.hamiltonian_cache, # system Hamiltonian
-                                          cache=True,                        # whether to cache calculations (faster)
-                                          project=meas1["+"],                # projection operator for measurement
-                                          bloch=[True, 100])                # Whether to save pnts for bloch state 
-                                                                             # and save interval
-    end = time.time()
+		# evolve state under system Hamiltonian
+		start = time.time()
+		tdomain, probs, pnts = atom.state_evolve(t=[1e-44, eperiod, 1/fs],          # time range and step size to evolve for
+																					hamiltonian=ham.hamiltonian_cache, # system Hamiltonian
+																					cache=True,                        # whether to cache calculations (faster)
+																					project=meas1["+"],                # projection operator for measurement
+																					bloch=[True, 100])                # Whether to save pnts for bloch state 
+																																						 # and save interval
+		end = time.time()
 
 
-    # frame_anaylser(atom.state_cache, frmame)
+		# frame_anaylser(atom.state_cache, frmame)
 
-    print("Atomic state evolution over {} seconds with Fs = {:.2f} MHz took {:.3f} seconds".format(eperiod, fs/1e6, end-start))
-    
-    # Save timestamp for current sim
-    tStamp = time.strftime( "%Y%m%dT%H%M%S")
+		print("Atomic state evolution over {} seconds with Fs = {:.2f} MHz took {:.3f} seconds".format(eperiod, fs/1e6, end-start))
+		
+		# Save timestamp for current sim
+		tStamp = time.strftime( "%Y%m%dT%H%M%S")
 
-    # Define whether we want to save plots
-    savePlots = False
+		# Define whether we want to save plots
+		savePlots = True
 
-    # plot on Bloch sphere, saving timestamped filename if savePlots is true
-    fNameBloch = str(tStamp) + '_blochplot'
-    atom.bloch_plot(fNameBloch, pnts, savePlots)  
+		# plot on Bloch sphere, saving timestamped filename if savePlots is true
+		fNameBloch = str(tStamp) + '_blochplot'
+		atom.bloch_plot(fNameBloch, pnts, savePlots)  
 
-    # plot |<0|psi(t)>|^2 against time, saving timestamed filename if savePlots is true
-    # with commit ID annotated to plot
-    fNameProb = str(tStamp) + '_probplot'
-    atom.prob_plot(tdomain, probs, fNameProb, commitID, savePlots)
+		# plot |<0|psi(t)>|^2 against time, saving timestamed filename if savePlots is true
+		# with commit ID annotated to plot
+		fNameProb = str(tStamp) + '_probplot'
+		atom.prob_plot(tdomain, probs, fNameProb, commitID, savePlots)
 
-    # compute projection <Fx>
-    fxProj = 2*probs - 1;
+		# compute projection <Fx>
+		fxProj = 2*probs - 1;
 
-    # plot <F_x> against time, saving timestamed filename if savePlots is true
-    # with commit ID annotated to plot
-    fNameProb = str(tStamp) + '_projectionplot'
-    atom.project_plot(tdomain, fxProj, fNameProb, commitID, savePlots)
+		# plot <F_x> against time, saving timestamed filename if savePlots is true
+		# with commit ID annotated to plot
+		fNameProb = str(tStamp) + '_projectionplot'
+		atom.project_plot(tdomain, fxProj, fNameProb, commitID, savePlots)
 
-    # next, want to use James' code to try demodulate <Fx>
-    # timeTest = np.arange(1e-44, eperiod, 1/fs)
-    # testSignal = np.zeros(len(timeTest));
-    # for i in range(1, len(timeTest)):
-    #   t = timeTest[i]
-    #   testSignal[i] =  np.sin(2 * np.pi * f0 * t) #+ np.cos(2 * np.pi * rabi * t)
+		# next, want to use James' code to try demodulate <Fx>
+		if runDemod is True:
+			demodFxProj = demod_from_array(fxProj, faraday_sampling_rate = fs, reference_frequency = f0, \
+				lowpas_freq = fPass, plot_demod = True, save = savePlots, time_stamp = str(tStamp))
 
-    # plt.figure()
-    # plt.plot(timeTest, testSignal)
-    # plt.xlim(min(timeTest), max(timeTest))
-    # plt.show()
+		# show all plotted figures
+		plt.show()
 
-    demodFxProj = demod_from_array(fxProj, faraday_sampling_rate = fs, reference_frequency = f0, \
-     lowpas_freq = 15000, plot_demod = True, save = savePlots, time_stamp = str(tStamp))
+		# Export parameters to timestamped .yaml file as record of parameters used for shot
+		# can then load in to replicate 
+		# Note: Have added such that only saved when plots are saved
+		if savePlots is True:
+			fNameYaml = 'C:/Users/Boundsy/Documents/GitHub/Atomicpy/ParameterFiles/' \
+				 + str(tStamp) + '_params.yaml'
+			copyfile(fNameParams, fNameYaml)
 
-    plt.show()
-
-    # Export parameters to timestamped .yaml file as record of parameters used for shot
-    # can then load in to replicate 
-    # Note: Have added such that only saved when plots are saved
-    if savePlots is True:
-      fNameYaml = 'C:/Users/Boundsy/Documents/GitHub/Atomicpy/ParameterFiles/' \
-         + str(tStamp) + '_params.yaml'
-      stream = open(fNameYaml, 'w+')
-      yaml.dump({'stepFreq': fs,
-        'evolutionTime': eperiod,
-        'biasFreq': f0,
-        'detuningFreq': det,
-        'rabiFreq': rabi,
-        'phaseOffset': phi},
-         stream, default_flow_style=False)
-      stream.close()
-
-      print('Parameters exported to .yaml file in ParameterFiles subfolder')
+			print('Parameters exported to .yaml file in ParameterFiles subfolder')
